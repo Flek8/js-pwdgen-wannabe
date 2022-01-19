a3eff2910d6c41d248e863ef89c7c114d89c4bc5
Flavio Rodolfi <flek8rodolfi@gmail.com>
2022-01-19 16:27:08 +0100

    fatto

---
+++ main.js
@@ -0,0 +1,6 @@
+let nome = prompt('Qual è il tuo nome?');
+let cognome = prompt('Qual è il tuo cognome?');
+let colore = prompt('Qual è il tuo colore preferito');
+const numero = 21
+
+document.getElementById('password').innerHTML = 'La tua password è: ' + nome + cognome + colore + numero;
